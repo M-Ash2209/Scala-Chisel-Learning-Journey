027e25a4cca0dbfd34cc7b5d699a73a0a2adb756
M-ASH2209 <asharsaeed2209@gmail.com>
2021-10-21 15:52:02 +0500

    initial

--- src/main/scala/lab4/ALU.scala
+++ src/main/scala/lab4/ALU.scala
@@ -34,7 +34,6 @@ class ALUIO extends Bundle with Config {
     val out = Output(UInt(WLEN.W))
     val sum = Output(UInt(WLEN.W))
 }
-
 class ALU extends Module with Config {
     val io = IO(new ALUIO)
 
@@ -42,19 +41,19 @@ class ALU extends Module with Config {
     val cmp = Mux(io.in_A(WLEN-1) === io.in_B(WLEN-1), sum(WLEN-1),
                 Mux(io.alu_Op(1), io.in_A(WLEN-1), io.in_B(WLEN-1)))
     val shamt = io.in_A(4,0).asUInt
-    val shin = Mux(io.alu_Op(3), Reverse(io.in_A), io.in_A)
-    val shiftr = (Cat(io.alu_Op(0) && shin, shin(WLEN-1)).asSInt >> shamt)(WLEN-1,0)
-    val shitfl = Reverse(shiftr)
-    val out = 
-    Mux(io.alu_Op === ALU_ADD || io.alu_Op === ALU_SUB, sum, 
-    Mux(io.alu_Op === ALU_SLT || io.alu_Op === ALU_SLTU, cmp, 
-    Mux(io.alu_Op === ALU_SRA || io.alu_Op === ALU_SRL, shiftl,
-    Mux(io.alu_Op === ALU_SLL, shitfr, 
-    Mux(io.alu_Op === ALU_AND, (io.in_A && io.in_B),
-    Mux(io.alu_Op === ALU_OR, (io.in_A | io.in_B),
-    Mux(io.alu_Op === ALU_XOR, (io.in_A ^ io.in_B),
-    Mux(io.alu_Op === ALU_COPY_A, io.in_A, 
-    Mux(io.alu_Op === ALU_COPY_B, io.in_A, 0.U)))))))))
+    val shin = (Mux(io.alu_Op(3), Reverse(io.in_A), io.in_A))
+    val shiftr = (Cat(io.alu_Op(0) && shin(0), 0.U).asSInt >> shamt)(1,0)
+    val shiftl = Reverse(shiftr)
+    
+    val out = Mux(io.alu_Op === ALU_ADD || io.alu_Op === ALU_SUB, sum, 
+                Mux(io.alu_Op === ALU_SLT || io.alu_Op === ALU_SLTU, cmp, 
+                Mux(io.alu_Op === ALU_SRA || io.alu_Op === ALU_SRL, shiftl,
+                Mux(io.alu_Op === ALU_SLL, shiftr, 
+                Mux(io.alu_Op === ALU_AND, (io.in_A & io.in_B),
+                Mux(io.alu_Op === ALU_OR, (io.in_A | io.in_B),
+                Mux(io.alu_Op === ALU_XOR, (io.in_A ^ io.in_B),
+                Mux(io.alu_Op === ALU_COPY_A, io.in_A, 
+                Mux(io.alu_Op === ALU_COPY_B, io.in_A, 0.U)))))))))
 
 
     io.out := out
